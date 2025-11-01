92dc40f6b6e1b9320356a4b868f307506f840056
Leonni Gant <65195226+Asfiction@users.noreply.github.com>
2025-11-01 15:11:45 -0400

    1_9 Interface Builder

---
+++ Unit1/Lesson1_9/InterfaceBuilderBasics/InterfaceBuilderBasics/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  InterfaceBuilderBasics
+//
+//  Created by Lizzy on 11/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var mainLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeTitle(_ sender: Any) {
+        mainLabel.text = "This app rocks!"
+    }
+    
+}
+
